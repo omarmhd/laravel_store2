5a3fb5a8a7f527a141d6184d3176259cbfeace2b
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-06-15 11:45:30 +0300

    connection change

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url('postgres://rmoymgkwhwbtoe:82db7c353c5501dc5672f6d4d8a0fd2fe58ef53b91758cc790d4c234dc447d05@ec2-52-87-135-240.compute-1.amazonaws.com:5432/d77mj4vrc5e1qm');
 return [
 
     /*
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+           // 'url' => env('DATABASE_URL'),
+            'host' =>$DATABASE_URL['host'],
+            'port' =>$DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['host'],'/'),
+            'username' => $DATABASE_URL['user'],
+            'password' =>$DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
