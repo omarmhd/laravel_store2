cb0d1e8a4143f5b007691f352c319b381419d0aa
unknown <hamza.05926170@gmail.com>
2020-07-11 00:24:21 +0300

    seventh1 commit

--- resources/views/header/headerbot.blade.php
+++ resources/views/header/headerbot.blade.php
@@ -1,15 +1,15 @@
 <div class="header-bot">
 	<div class="header-bot_inner_wthreeinfo_header_mid">
 		<div class="col-md-4 header-middle">
-			<form action="#" method="post">
+			{{-- <form action="#" method="post">
 					<input type="search" name="search" placeholder="Search here..." required="">
 					<input type="submit" value=" ">
 				<div class="clearfix"></div>
-			</form>
+			</form> --}}
 		</div>
 		<!-- header-bot -->
 			<div class="col-md-4 logo_agile">
-				<h1><a href="index.html"><span>حرفتي</span> <i class="fa fa-shopping-bag top_logo_agile_bag" aria-hidden="true"></i></a></h1>
+				<h1><a href="{{ route('home.index') }}"><span>حرفتي</span> <i class="fa fa-shopping-bag top_logo_agile_bag" aria-hidden="true"></i></a></h1>
 			</div>
         <!-- header-bot -->
 		<div class="col-md-4 agileits-social top_content">
--- resources/views/header/menu.blade.php
+++ resources/views/header/menu.blade.php
@@ -9,10 +9,18 @@
     font-size: 24px;
     color: #000;
 }
+.top_nav_left {
+    float: left;
+    width: 21%;
+}
+.top_nav_right {
+    float: right;
+    width:auto !important;
+}
 </style>
 <div class="ban-top">
 	<div class="container">
-		<div class="top_nav_left">
+		<div class="top_nav_right">
 			<nav class="navbar navbar-default">
 			  <div class="container-fluid">
 				<!-- Brand and toggle get grouped for better mobile display -->
@@ -25,12 +33,12 @@
 				  </button>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse menu--shylock" id="bs-example-navbar-collapse-1">
+				<div class="collapse navbar-collapse menu--shylock" id="bs-example-navbar-collapse-1" >
 				  <ul class="nav navbar-nav menu__list">
 				  <li class="active menu__item {{isset($current) && $current  == 'home' ? 'menu__item--current':''}}"><a class="menu__link" href="{{route('home.index')}}">الرئيسية <span class="sr-only">(current)</span></a></li>
 				  <li class=" menu__item {{isset($current) && $current  == 'about' ? 'menu__item--current':''}}"><a class="menu__link" href="{{ route('about.index') }}">من نحن</a></li>
-					<li class=" menu__item"><a class="menu__link" href="about.html">الأعمال</a></li>
-					<li class="dropdown menu__item">
+					{{-- <li class=" menu__item"><a class="menu__link" href="about.html">الأعمال</a></li> --}}
+					{{-- <li class="dropdown menu__item">
 						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">الصفحات <span class="caret"></span></a>
 							<ul class="dropdown-menu multi-column columns-3">
 								<div class="agile_inner_drop_nav_info">
@@ -47,7 +55,7 @@
 									<div class="clearfix"></div>
 								</div>
 							</ul>
-					</li>
+					</li> --}}
 				<li class=" menu__item"><a class="menu__link {{isset($current) && $current  == 'orders' ? 'menu__item--current':''}}" href="{{route('show.status')}}">طلباتي</a></li>
 				<li class=" menu__item {{isset($current) && $current  == 'contact' ? 'menu__item--current':''}}"><a class="menu__link" href="{{route('contact.index')}}">اتصل بنا</a></li>
 				  </ul>
@@ -55,7 +63,7 @@
 			  </div>
 			</nav>	
 		</div>
-		<div class="top_nav_right">
+		<div class="top_nav_left">
 			<div class="wthreecartaits wthreecartaits2 cart cart box_1"> 
 				
 
